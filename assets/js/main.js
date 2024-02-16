10b6cfd3c20296e5b7abbfa37693b95659411907
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-02-16 15:19:54 +0100

    add exercise 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
 // EXERCISE 1
-const number = 100;
+const number = 100; // number
 console.log(number);
+
+// EXERCISE 2/A
+const PI = 3.14; // number
+console.log(PI);
+
+// EXERCISE 2/B
+// PI = 3.1416; // Non è possibile cambiare il contenuto di una variabile inizializzata come una costante, Quindi la console mi segnala l'errore
+
+// EXERCISE 2/C
+const radius = 8; //number
+const circle = radius*2*PI; //number
+console.log(circle);
+
